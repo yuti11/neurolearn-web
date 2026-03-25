b4fcfac142cc6b9ffbd95b3d35d6acef8f5657ae
Yutika.N <yutika740@gmail.com>
2026-03-25 20:34:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,32 +11,62 @@ return;
 let data = {
 "Data Scientist": {
 skills: "Python, Machine Learning, Statistics",
-courses: "ML Basics, Pandas, Data Science Intro"
+courses: "ML Basics, Pandas, Data Science Intro",
+values: [5,4,3]
 },
 "Web Developer": {
 skills: "HTML, CSS, JavaScript",
-courses: "Web Development, Frontend Basics, JS Advanced"
+courses: "Web Development, Frontend Basics, JS Advanced",
+values: [5,4,3]
 },
 "Data Analyst": {
 skills: "Excel, SQL, Python",
-courses: "Data Analysis, SQL Basics, Excel Advanced"
+courses: "Data Analysis, SQL Basics, Excel Advanced",
+values: [5,4,3]
 },
 "AI Engineer": {
 skills: "Python, Deep Learning, TensorFlow",
-courses: "Deep Learning, Neural Networks, AI Basics"
+courses: "Deep Learning, Neural Networks, AI Basics",
+values: [5,4,3]
 },
 "Software Engineer": {
 skills: "Java, DSA, OOP",
-courses: "Data Structures, Algorithms, OOP Concepts"
+courses: "Data Structures, Algorithms, OOP Concepts",
+values: [5,4,3]
 }
 };
 
 let result = data[role];
 
+// TEXT OUTPUT
 document.getElementById("result").innerHTML =
 "<b>Student ID:</b> " + id +
 "<br><b>Career:</b> " + role +
 "<br><b>Skills:</b> " + result.skills +
 "<br><b>Courses:</b> " + result.courses;
+
+
+// GRAPH
+
+let ctx = document.getElementById('chart').getContext('2d');
+
+// destroy old chart if exists (important)
+if(window.myChart){
+window.myChart.destroy();
+}
+
+window.myChart = new Chart(ctx, {
+type: 'bar',
+data: {
+labels: result.skills.split(", "),
+datasets: [{
+label: 'Skill Importance',
+data: result.values
+}]
+},
+options: {
+responsive: true
+}
+});
 
 }
